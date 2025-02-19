54b5238feb910547042b781aaa5184561fb82b51
ThomasMoly <160690781+ThomasMoly@users.noreply.github.com>
2025-02-19 16:18:04 -0500

    Organized Code
    Organized Test folder with a new Constants.java file that holds constants and nothing else

---
+++ Test/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+public class Constants extends TimedRobot {
+
+    // Motor controllers for the left and right drive motors
+    public static final PWMSparkMax m_leftDrive1 = new PWMSparkMax(0); // back left
+    public static final PWMSparkMax m_leftDrive2 = new PWMSparkMax(1); // front left
+    public static final PWMSparkMax m_rightDrive1 = new PWMSparkMax(2); // front right
+    public static final PWMSparkMax m_rightDrive2 = new PWMSparkMax(3); // back right
+    
+    public static final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive2::set, m_rightDrive1::set);
+    public static final XboxController m_controller = new XboxController(0);
+    public static final Timer m_timer = new Timer();
+}
