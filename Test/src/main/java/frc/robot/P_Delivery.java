69f32e79d1b22c00dfdbad23b31c02d58afc6c66
ThomasMoly <160690781+ThomasMoly@users.noreply.github.com>
2025-02-20 06:41:13 -0500

    Create P_Delivery.java

---
+++ Test/src/main/java/frc/robot/P_Delivery.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+public class P_Delivery extends TimedRobot {
+    //Kit-Bot Delivery System for the pipes
+  public static final PWMSparkMax P_delivery = Constants.P_Delivery;
+}
