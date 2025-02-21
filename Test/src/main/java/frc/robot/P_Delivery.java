474c5d86506fb9b89fe1916559c82e3dadcfc018
ThomasMoly <160690781+ThomasMoly@users.noreply.github.com>
2025-02-20 21:08:44 -0500

    Update P_Delivery.java

--- Test/src/main/java/frc/robot/P_Delivery.java
+++ Test/src/main/java/frc/robot/P_Delivery.java
@@ -10,8 +10,17 @@ public class P_Delivery extends TimedRobot {
     //Kit-Bot Delivery System for the pipes
     public static final PWMSparkMax P_delivery = Constants.P_motorDelivery;
 
-    public static boolean Autonmous(){
-    
+    @Override
+    public void Autonomous(){
+        //should set the speed of the motor for the delivery system
+        P_delivery.set(1); 
+    }
+
+    @Override
+    public void Teleop{
+        // Control the motor with the controller trigger
+        double speed = controller.getRightTriggerAxis(); // Adjust based on control scheme
+        P_delivery.set(speed); // Runs the motor with trigger input
     }
     
 }
