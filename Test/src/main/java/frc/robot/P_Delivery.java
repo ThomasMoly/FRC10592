bedbe9ccc75a1d29d3daa8a42c84d6d0474ad470
ThomasMoly <160690781+ThomasMoly@users.noreply.github.com>
2025-02-24 11:08:19 -0500

    Update P_Delivery.java

--- Test/src/main/java/frc/robot/P_Delivery.java
+++ Test/src/main/java/frc/robot/P_Delivery.java
@@ -6,19 +6,19 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
-public class P_Delivery extends  {
+public class P_Delivery{
     //Kit-Bot Delivery System for the pipes
     public static final PWMSparkMax P_delivery = Constants.P_motorDelivery;
     public static final XboxController controller = Constants.m_controller;
 
     @Override
-    public void Autonomous(){
+    public void autonomousInit(){
         //should set the speed of the motor for the delivery system
         P_delivery.set(1); 
     }
 
     @Override
-    public void Teleop(){
+    public void teleopInit(){
         // Control the motor with the controller trigger
         double speed = controller.getRightTriggerAxis(); // Adjust based on control scheme
         P_delivery.set(speed); // Runs the motor with trigger input
