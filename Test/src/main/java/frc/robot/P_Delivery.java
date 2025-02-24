f8bc5a97976a203202c0ed174fb4054134197303
ThomasMoly <160690781+ThomasMoly@users.noreply.github.com>
2025-02-24 10:18:58 -0500

    Update P_Delivery.java

--- Test/src/main/java/frc/robot/P_Delivery.java
+++ Test/src/main/java/frc/robot/P_Delivery.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
-public class P_Delivery extends TimedRobot {
+public class P_Delivery extends  {
     //Kit-Bot Delivery System for the pipes
     public static final PWMSparkMax P_delivery = Constants.P_motorDelivery;
     public static final XboxController controller = Constants.m_controller;
